20653a7c4c671117d6b891e568dfd2345440c8fb
Masa Sakano <masasakano@users.noreply.github.com>
2022-08-26 19:09:00 +0100

    Initial commit.

---
+++ lib/pandoc_refeq_mathml.rb
@@ -0,0 +1,189 @@
+# coding: utf-8
+
+require 'nokogiri'
+
+# Class to handle MathML and LaTeX aux
+#
+class PandocRefeqMathml
+
+  attr_reader :page, :aux
+
+  # Alignment for LaTeX eqnarray.
+  EQNARRAY_ALIGNS = %w(right center left)
+
+  # @param page [Nokogiri::HTML4::Document]
+  # @param auxstr [String] String of the contents of a LaTeX aux file
+  # @param logio [IO] Output IO for logs. You may give +IO.open("/dev/null", "w")+. Def: $stderr
+  # @param is_verbose [Boolean] If true (Default), verbose.
+  def initialize(page, auxstr, logio: $stderr, is_verbose: true)
+    @page = page
+    @auxstr = auxstr
+    @logio = logio
+    @is_verbose = is_verbose
+    @hslabel = {}  # String(label) => String(EqNumber), which have been detected.
+  end
+
+  # @param taga [Nokogiri::HTML4::Document] Nokogiri <A> object.
+  # @return [String, NilClass] Label string used in +<a href=..>+. Nil if something is wrong.
+  def get_label(taga)
+    kwdlink = taga['href'].split('#')[1]
+    return kwdlink if kwdlink == taga['data-reference']
+
+    @logio.puts "WARNING: Inconsistent href and data-reference: "+a.to_s if @is_verbose
+    return nil
+  end
+
+  # Return the equation number.
+  #
+  # The number is assumed to contain only numbers and maybe full-stops +[\d.]+.
+  #
+  # @return [String, NilClass] Equation number guessed from the Aux file. nil if something goes wrong.
+  def get_eq_num(kwdlink)
+    mat = /^\\newlabel\{#{kwdlink}\}\{\{([\d\.]+)\}\{(\d+)\}\{(.*)\}\{equation.([a-zA-Z\d\.]+)\}/.match @auxstr  # it may be like equation.B.193 (for Appendix B).
+       # => #<MatchData "\\newlabel{eq_my_lab}{{65}{35}{割り算}{equation.4.62}" 1:"65" 2:"35" 3:"割り算" 4:"4.62">
+    return mat[1] if mat && mat[1] && !mat[1].empty?
+
+    ## Something is wrong. 
+    str = sprintf 'WARNING: Not found equation number for label="%s" (maybe it is for a section etc?): MatchData=%s', kwdlink, mat.inspect
+    @logio.puts str
+    return nil
+  end
+
+  # @param kwdlink [String] label
+  # @param n_eq [String] Equation number like "58", maybe "52.3"
+  # @return [Integer, NilClass] If something goes wrong, 
+  def find_insert_n_eq(kwdlink, n_eq)
+    # Select the <math> tag component that hs the kwdlink
+    maths = @page.css('math').select{|ep|
+      /\\label\{\s*#{Regexp.quote(kwdlink)}\s*\}/ =~ (ep.css('annotation[encoding="application/x-tex"]').children[0].text.strip rescue "X")
+    }
+    if maths.size != 1
+      if maths.size == 0
+        @logio.puts 'WARNING: no math tag contains label="#{kwdlink}"'
+      else
+        @logio.print 'WARNING: Multiple math tags contain label="#{kwdlink}"'
+        @logio.puts (@is_verbose ? ": maths="+maths.inspect : "")
+      end
+      return nil
+    end
+
+    mtext = sprintf '<mtext id="%s" style="padding-left:1em; text-align:right;">(%s)</mtext>', kwdlink, n_eq
+    
+    if maths[0].css('mtable').empty?
+      # \begin{equation}
+      #
+      # Insert the new node (<mrow><mtext...>(65)</mtext></mrow>) as the last child of
+      # the last top-level existing <mrow>; if it is added AFTER the <mrow>
+      # the <mtext> number would not be displayed!
+      newnode = '<mrow>' + mtext + '</mrow>'
+      begin
+        maths[0].css("mrow")[0].parent.css("> mrow")[-1].add_child(newnode)
+          # Between the last <mrow> and <annotation>
+          # n.b., css('mrow')[-1] would give an mrow inside another mrow!
+      rescue
+        msg = "FATAL: contact the code developer: equation: maths[0]="+maths[0].inspect
+        @logio.puts msg
+        raise msg
+      end
+      return 0
+    else
+      # \begin{eqnarray}
+      newnode = '<mtd columnalign="right">' + mtext + '</mtd>'
+      annot_node= maths[0].css('annotation[encoding="application/x-tex"]')[0]
+      i_eq_annot = annot_node.children[0].text.split(/\\\\\s*(?:\%[^\n]*)?\n?/).find_index{|ev| /\\label\{#{kwdlink}\}/ =~ ev}  # Index of the equation (starting from 0) in the eqnarray
+      raise "FATAL: contact the code developer: eqnarray: "+annot_node.inspect if !i_eq_annot
+
+      # Insert the new node (<mtd><mtext...>(65)</mtext></mtd>)
+      mtrnode = maths[0].css('mtable mtr')[i_eq_annot]
+      # mtrnode.css('mtd')[-1].add_next_sibling(newnode)  # not considering multi-layer tables
+      find_last_shallowest(mtrnode, 'mtd').add_next_sibling(newnode)
+    end
+    return i_eq_annot+1
+  end
+
+  # @param kwdlink [String] label
+  # @param taga [Nokogiri::HTML4::Document] Nokogiri <A> object.
+  # @param n_eq_str [String] equation number string
+  # @return [void]
+  def alter_link_text(kwdlink, taga, n_eq_str)
+    textnode = taga.children[0]
+    if !textnode.text?
+      @logio.puts "WARNING: Inconsistent text inside href: "+taga.to_s
+      return nil
+    end
+
+    if /\A\[?#{Regexp.quote(kwdlink)}\]?\z/ !~ textnode.to_s.strip
+      @logio.puts "WARNING: Strange linked-text=(#{textnode.to_s}) inside <a>: "+a.to_s if @is_verbose
+    end
+
+    taga.content=n_eq_str
+  end
+
+  # Alter the alignments of mtable originating from eqnarray
+  #
+  # Original is all right-aligned.
+  # After alteration, it will be right, center, left (which is the specification of eqnarray).
+  def alter_align_eqnarray!
+    @page.css("math mtable mtr").each do |ea_mtr|
+      ea_mtr.css("mtd").each_with_index do |ea_mtd, i|
+        break if i >= EQNARRAY_ALIGNS.size
+        ea_mtd["columnalign"]=EQNARRAY_ALIGNS[i]
+      end
+    end
+  end
+
+  # Alter the existing HTML Nokogiri content
+  def alter_reflinks!
+    all_ref_href = @page.css("a[data-reference-type=ref]")
+    all_ref_href.each do |ea_nodes|
+      # Gets a label from MathML
+      (kwdlink = get_label(ea_nodes)) || next
+
+      if !@hslabel.keys.include? kwdlink
+        # Gets the number of the equation from Aux
+        n_eq = get_eq_num(kwdlink)
+        next if !n_eq
+        @hslabel[kwdlink] = n_eq
+
+        # Finds the equation in MathML and adds the number of the equation.
+        find_insert_n_eq(kwdlink, n_eq) # this returns nil if something goes wrong
+      elsif !@hslabel[kwdlink]
+        # the label "kwdlink" has been detected, but no Equation-number was found.
+        next
+      end
+
+      # Alter the original link text in MathML to Equation number.
+      alter_link_text(kwdlink, ea_nodes, @hslabel[kwdlink])
+    end
+  end
+
+  # @param fixalign [Boolean] fix alignments if true
+  def alter_html!(fixalign: true)
+    alter_align_eqnarray! if fixalign
+    alter_reflinks!
+  end
+
+  # Returns the last shallowest node with the given tag-name
+  #
+  # @see https://stackoverflow.com/a/73459162/3577922
+  #
+  # @param root [NokogiriXmlNode] root node
+  # @param tagname [String] Tag-name like "mrow" for which the last-shallowest is looked
+  def find_last_shallowest(root, tagname)
+    queue = [root]
+    while queue.any?
+      element = queue.shift
+      return element if node_matching?(element, tagname)
+      queue.concat element.children.reverse
+    end
+  end
+  private :find_last_shallowest
+
+  # @param element [NokogiriXmlNode] including Nokogiri::XML::Element
+  def node_matching?(element, tagname)
+    # Put your matching logic here
+    element.name == tagname
+  end
+  private :node_matching?
+end # PandocRefeqMathml
+
---
+++ test/test_pandoc_refeq_mathml.rb
@@ -0,0 +1,188 @@
+# -*- encoding: utf-8 -*-
+#
+# Usage(at-Parent-directory): RUBYLIB=$RUBYLIB:./lib ruby test/test_pandoc_refeq_mathml.rb
+
+require 'open3'
+require 'tempfile'
+require 'diff/lcs'
+require 'diff/lcs/string'
+
+$stdout.sync=true
+$stderr.sync=true
+# print '$LOAD_PATH=';p $LOAD_PATH
+arlibrelpath = []
+arlibbase = %w(pandoc_refeq_mathml)	# Write those that are loaded inside a library to make their absolute paths be displayed.
+
+arlibbase.each do |elibbase|
+  arAllPaths = []
+  er=nil
+  pathnow = nil
+  (['../lib/', 'lib/', ''].map{|i| i+elibbase+'/'} + ['']).each do |dir|
+    begin
+      s = dir+File.basename(elibbase)
+      arAllPaths.push(s)
+      require s
+      pathnow = s
+      break
+    rescue LoadError => er
+    end
+  end	# (['../lib/', 'lib/', ''].map{|i| i+elibbase+'/'} + '').each do |dir|
+
+  if pathnow.nil?
+    warn "Warning: All the attempts to load the following files have failed.  Abort..."
+    warn arAllPaths.inspect
+    warn " NOTE: It may be because a require statement in that file failed, 
+rather than requiring the file itself.
+ Check with  % ruby -r#{File.basename(elibbase)} -e p
+ or maybe add  env RUBYLIB=$RUBYLIB:`pwd`"
+    # p $LOADED_FEATURES.grep(/#{Regexp.quote(File.basename(elibbase)+'.rb')}$/)
+    raise er
+  else
+#print pathnow," is loaded!\n"
+    arlibrelpath.push pathnow
+  end
+end	# arlibbase.each do |elibbase|
+
+print "NOTE: Library relative paths: "; p arlibrelpath
+print "NOTE: Library full paths:\n"
+arlibbase.each do |elibbase|
+  p $LOADED_FEATURES.grep(/#{Regexp.quote(File.basename(elibbase)+'.rb')}$/)
+end
+
+
+
+#################################################
+# Unit Test
+#################################################
+
+gem "minitest"
+# require 'minitest/unit'
+require 'minitest/autorun'
+# MiniTest::Unit.autorun
+
+class TestUnitPandocRefeqMathml < MiniTest::Test
+  T = true
+  F = false
+
+  def setup
+    @exefile  = __dir__ + "/../bin/pandoc_refeq_mathml"
+    @auxfile  = __dir__ + "/data/try01_latex.aux"
+    @htmlfile = __dir__ + "/data/try01.html"
+
+    # Array of IOs for temporary files (automatically set in generate_tmpfile())
+    @tmpfiles = []
+  end
+
+  def teardown
+    @tmpfiles.each do |ef|
+      ef.close if !ef.closed? 
+      File.unlink(ef.path)
+    end
+  end
+
+  # @option root [#to_s] Root-name of the temporary filename
+  def generate_tmpfile(root=File.basename($0))
+    io_tmpfile = Tempfile.open(root.to_s)
+    $stderr.print "TEST: Tmpfile="+io_tmpfile.path if ENV.key?('PRINT_TMPFILE') # To display Filename (NOTE the file will be removed when the script ends anyway.)
+    @tmpfiles.push io_tmpfile
+    [io_tmpfile, io_tmpfile.path]
+  end
+
+  def test_pandoc_refeq_mathml
+    auxstr = File.read @auxfile
+    htmlstr = File.read @htmlfile
+    page00 = Nokogiri::HTML(htmlstr)
+    page   = Nokogiri::HTML(htmlstr)
+    io_tmp, path_tmp = generate_tmpfile(__method__)
+
+    prm = PandocRefeqMathml.new page, auxstr, logio: io_tmp, is_verbose: true
+    prm.alter_html!
+
+    # an Equation (LaTeX: \begin{equation})
+    math1_org = page00.css("math:first-of-type")[0]
+    math1_rev = prm.page.css("math:first-of-type")[0]
+    lcs = math1_org.to_s.diff(math1_rev.to_s)
+    assert_equal 1, lcs.size, 'Diff-size should be 1 (one continuous addition only)'
+    assert_operator 90, '<', lcs[0].size, 'Number of different characters should be larger than 90'
+    assert_operator 99, '>', lcs[0].size, 'Number of different characters should be smaller than 99: Diff='+mk_str_diff_chg(lcs).inspect  # "mrow><mtext id=\"square_pm\" style=\"padding-left:1em; text-align:right;\">(36)</mtext></mrow><"
+    assert((%r@</mtext>@ !~ math1_org.to_s), '</mtext> should not be included')
+    assert_match(%r@</mtext></mrow></mrow>@, math1_rev.to_s, '</mtext></mrow></mrow> should be included')
+
+    mtds = prm.page.css("math mtable mtr")[2].css("mtd")
+    assert_equal "right",  mtds[0]["columnalign"]
+    assert_equal "center", mtds[1]["columnalign"], "align should be center: "+mtds[1]
+    assert_equal "left",   mtds[2]["columnalign"]
+    # NOTE: --no-fixalign is tested in test_integrated()
+
+    io_tmp.rewind
+    msg_log = io_tmp.read
+    assert_match(%r@label=.?sec_@, msg_log, "Warning message should be present in the log file because Equation-ID is not found for a label for a Section: \n> "+msg_log)
+  end
+
+  # Integrated tests
+  def test_integrated
+    #com = sprintf "%s --aux=%s --log=%s ", @exefile, @auxfile, @logfilename
+    com = sprintf "%s --aux=%s --no-fixalign", @exefile, @auxfile  # Logfile => STDERR, fixalign=no
+
+    ## From STDIN, out to STDOUT, log-file to STDERR
+    out, err, stat = Open3.capture3(com, stdin_data: File.read(@htmlfile))
+    assert_equal 0, stat
+    assert_match(%r@label=.?sec_@, err, "Warning message should be present in STDERR because Equation-ID is not found for a label for a Section: \n> "+err)
+    assert_operator 5, '<=', out.scan(%r@(?=</mtext>)@).count, 'There should be many </mtext>. out[0..100]='+out[0..100]
+    assert_match(%r@\bcolumnalign="right"@,  out, 'Sanity check columnalign')
+    refute_match(%r@\bcolumnalign="center"@, out, 'With --no-fixalign center columnalign should not exist, but..')
+  end
+
+  # Read a 2-dim Array of Diff::LCS::Change and convert it to a single Array of them
+  #
+  # Each Diff may (or usually) have more than 1 character.
+  # And therefore, it should be far more readable for humans.
+  # Here is an example.
+  #
+  #   # [#<Diff::LCS::Change: ["+", 1, "x"]>, #<Diff::LCS::Change: ["+", 2, "y"]>]
+  #   # => <Diff::LCS::Change: ["+", 1, "xy"]>
+  #
+  # You can still patch it.
+  #
+  #   s2 == s1.patch( [mk_str_diff_chg(Diff::LCS.diff(s1, s2))] )
+  #
+  # However, +s2.unpatch [mk_str_diff_chg(...)]+ raises RuntimeError.
+  # I think it works by starting from the beginning, swapping "`+`" and "`-`",
+  # where interpreting "`-`"+0 as inserting before pos=0 and "`+`"+1 as deleting after pos=1.
+  #
+  # @param arlcs [Array<Array<<Diff::LCS::Change>>]
+  # @return [Array<Diff::LCS::Change>]
+  def mk_str_diff_chg(ar2lcs)
+    arlcs = []  # ar2lcs.flatten actually also flattens the contents of Diff::LCS::Change !
+                # Therefore, this is a custom Array#flatten
+    ar2lcs.each do |ea1|
+      ea1.each do |ea2|
+        arlcs.push ea2
+      end
+    end
+
+    return arlcs if arlcs.empty?
+
+    pos = pos_ini = arlcs[0].position - 99
+    strdiff       = nil
+    action_now    = nil
+
+    arret = []
+    arlcs.each do |ed|  # ed: EachDiff
+      if (pos != ed.position - 1) || (ed.action != action_now)
+        # The previous series has ended.
+        arret.push Diff::LCS::Change.new(action_now, pos_ini, strdiff) if action_now # unless the very first one
+        pos = pos_ini = ed.position
+        strdiff       = ed.element.dup
+        action_now    = ed.action
+        next
+      end
+
+      pos = ed.position
+      strdiff << ed.element
+    end
+    arret.push Diff::LCS::Change.new(action_now, pos_ini, strdiff)
+    arret
+  end
+end	# class TestUnitPandocRefeqMathml < MiniTest::Test
+
